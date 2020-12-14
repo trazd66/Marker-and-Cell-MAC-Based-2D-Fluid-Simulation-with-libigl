da3a794ba34ad22439a86207a41db2b08089ad15
Allen Bai <carpe.diem.allen@gmail.com>
2020-12-14 17:13:34 -0500

    grid: rewrite to grid functions
    Signed-off-by: Allen Bai <carpe.diem.allen@gmail.com>

--- src/grid.cpp
+++ src/grid.cpp
@@ -7,16 +7,18 @@ Eigen::MatrixXd InitGridPressures(const int rows, const int cols)
     return result;
 }
 
-Eigen::MatrixXd InitGridVelocityU(const int rows, const int cols); {
+Eigen::MatrixXd InitGridVelocityU(const int rows, const int cols)
+{
     /* need to add one since num_borders = num_grid + 1 */
-    Eigen::MatrixXd result(rows, cols+1);
+    Eigen::MatrixXd result(rows, cols + 1);
     result.setZero();
     return result;
 }
 
-Eigen::MatrixXd InitGridVelocityV(const int rows, const int cols) {
+Eigen::MatrixXd InitGridVelocityV(const int rows, const int cols)
+{
     /* need to add one since num_borders = num_grid + 1 */
-    Eigen::MatrixXd result(rows+1, cols);
+    Eigen::MatrixXd result(rows + 1, cols);
     result.setZero();
     return result;
 }

e6f22034cff47572214162ed03f27738e92c708d
trazd66 <trazd66@trazd66.trazd66>
2020-12-17 01:37:59 -0800

    fixing indexing erros

--- include/utilities.h
+++ include/utilities.h
@@ -6,6 +6,8 @@
  * 
  * transates the x,y position from the paper into eigen indexes
  * 
+ * e.g.
+ *get_matrix_index_2d(x_non_staggered,y-1,x_len_non_staggered,y_len,i_idx,j_idx);
  * 
  ***/
 void get_matrix_index_2d(const int x ,const int y, 

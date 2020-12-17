e4b1e05b987930dbe1ac4a3408c7bddbd51641e0
Allen Bai <carpe.diem.allen@gmail.com>
2020-12-17 00:20:36 -0500

    use get_matrix_index_2d
    Signed-off-by: Allen Bai <carpe.diem.allen@gmail.com>

--- src/particle_to_grid.cpp
+++ src/particle_to_grid.cpp
@@ -7,7 +7,7 @@
     https://en.wikipedia.org/wiki/Bilinear_interpolation#:~:text=In%20mathematics%2C%20bilinear%20interpolation%20is,again%20in%20the%20other%20direction.
     EFFECT: Updates M_v
 */
-void v_particle_onto_grid_v(Eigen::MatrixXd &M_v, Eigen::Vector2d &pos_particle, double v_particle, double interval_x, double interval_y)
+void v_particle_onto_grid_v(Eigen::MatrixXd &M_v, Eigen::Vector2d &pos_particle, double v_particle, double interval_x, double interval_y, const int len_x,const int len_y)
 {
     assert(interval_x * interval_y != 0 && "intervals should not be 0.");
     /* pos_particle -> (x,y) */
@@ -17,7 +17,12 @@ void v_particle_onto_grid_v(Eigen::MatrixXd &M_v, Eigen::Vector2d &pos_particle,
     int grid_y_start = (int)(particle_y / interval_y);
     int grid_x_end = grid_x_start + 1;
     int grid_y_end = grid_y_start + 1;
-    //63 , 23
+
+    get_matrix_index_2d(grid_x_start, grid_y_start, len_x, len_y, grid_x_start, grid_y_start);
+    get_matrix_index_2d(grid_x_start, grid_y_end, len_x, len_y, grid_x_start, grid_y_end);
+    get_matrix_index_2d(grid_x_end, grid_y_start, len_x, len_y, grid_x_end, grid_y_start);
+    get_matrix_index_2d(grid_x_end, grid_y_end, len_x, len_y, grid_x_end, grid_y_end);
+
     std::cout << grid_x_start << " " << grid_y_start << " " << grid_x_end << " " << grid_y_end << '\n';
     M_v(grid_y_start, grid_x_start) += (1 / (interval_x * interval_y)) * (grid_x_end * interval_x - particle_x) * (grid_y_end * interval_y - particle_y) * v_particle;
     M_v(grid_y_start, grid_x_end) += (1 / (interval_x * interval_y)) * (grid_x_end * interval_x - particle_x) * (particle_y - grid_y_start * interval_y) * v_particle;
@@ -33,7 +38,7 @@ void v_particle_onto_grid_v(Eigen::MatrixXd &M_v, Eigen::Vector2d &pos_particle,
     NOTE: the same logic as v_particle_onto_grid_v, can merge into one if we want.
     EFFECT: Updates M_u
 */
-void u_particle_onto_grid_u(Eigen::MatrixXd &M_u, Eigen::Vector2d &pos_particle, double u_particle, double interval_x, double interval_y) {
+void u_particle_onto_grid_u(Eigen::MatrixXd &M_u, Eigen::Vector2d &pos_particle, double u_particle, double interval_x, double interval_y, const int len_x,const int len_y) {
     assert(interval_x * interval_y != 0 && "intervals should not be 0.");
     /* pos_particle -> (x,y) */
     double particle_x = pos_particle[0];
@@ -42,6 +47,11 @@ void u_particle_onto_grid_u(Eigen::MatrixXd &M_u, Eigen::Vector2d &pos_particle,
     int grid_y_start = (int)((particle_y - 0.5 * interval_y) / interval_y);
     int grid_x_end = grid_x_start + 1;
     int grid_y_end = grid_y_start + 1;
+
+    get_matrix_index_2d(grid_x_start, grid_y_start, len_x, len_y, grid_x_start, grid_y_start);
+    get_matrix_index_2d(grid_x_start, grid_y_end, len_x, len_y, grid_x_start, grid_y_end);
+    get_matrix_index_2d(grid_x_end, grid_y_start, len_x, len_y, grid_x_end, grid_y_start);
+    get_matrix_index_2d(grid_x_end, grid_y_end, len_x, len_y, grid_x_end, grid_y_end);
 
     M_u(grid_y_start, grid_x_start) += (1 / (interval_x * interval_y)) * (grid_x_end * interval_x - particle_x) * (grid_y_end * interval_y - particle_y) * u_particle;
     M_u(grid_y_start, grid_x_end) += (1 / (interval_x * interval_y)) * (grid_x_end * interval_x - particle_x) * (particle_y - grid_y_start * interval_y) * u_particle;

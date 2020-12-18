9a5e48f25d36a3d8f77339f0c0e17ebf8c82d0c0
trazd66 <trazd66@trazd66.trazd66>
2020-12-17 19:58:35 -0800

    fixing indexing erros

--- main.cpp
+++ main.cpp
@@ -26,11 +26,11 @@ bool is2d = true;
 double rho = 1.0; // density of water
 double t = 0;      //simulation time
 double dt = 0.005; //time step
-Eigen::Vector2d g(0., -0.0098); // gravity acceleration
+Eigen::Vector2d g(0., -9.8); // gravity acceleration
 
 const int bb_size_x = 50; // x dimension of the bounding box -> number of grids in x axis
 const int bb_size_y = 50; // y dimension of the bounding box -> number of grids in y axis
-const double grid_interval = 5.; // size of the grid interval, determines the number of grid cells
+const double grid_interval = 0.1; // size of the grid interval, determines the number of grid cells
 const int num_particles = 1; // number of particles
 Eigen::MatrixXd M_particles; // the particle matrix
 Eigen::VectorXd M_particles_u; // particle velocity u
